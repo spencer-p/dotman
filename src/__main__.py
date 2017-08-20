70236a8a2bbfb5e976a95cd9b86e800a2a6c49f6
Spencer Peterson <spencerjpeterson@gmail.com>
2017-08-19 18:12:20 -0700

    add init action

--- src/__main__.py
+++ src/__main__.py
@@ -29,6 +29,13 @@ def main():
     update_parser.add_argument('-p', '--package', help='package to filter and update')
     update_parser.set_defaults(func=actions.update)
 
+    # Dummy init action
+    init_parser = subparsers.add_parser('init',
+            description='initializes the config file if necessary.\
+            initialization happens automatically for other actions.',
+            help='initialize config if necessary')
+    init_parser.set_defaults(func=lambda args: None)
+
     # Parse arguments
     args = parser.parse_args()
 
