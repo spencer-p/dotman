7c24182dc5a83e3497ce4f0b7dc72f7bc2a78c60
Spencer Peterson <spencerjpeterson@gmail.com>
2017-08-19 18:03:28 -0700

    restructure main into function

--- src/__main__.py
+++ src/__main__.py
@@ -1,32 +1,46 @@
+import sys
 import argparse
 import actions
+import config
 
-# Name of program
-# TODO think of a good one
-NAME = 'dotman'
+def main():
+    # Create top level parser
+    parser = argparse.ArgumentParser(prog=config.NAME,
+            description="{} manages and filters your dotfiles.".format(config.NAME))
 
-# Create top level parser and add a subparser
-parser = argparse.ArgumentParser(prog=NAME,
-        description="{} manages and filters your dotfiles.".format(NAME))
-subparsers = parser.add_subparsers(help='action to take', dest='action')
+    # Multiple config option
+    parser.add_argument('-c', '--config', help='custom config file')
 
-# Parser for add action
-add_parser = subparsers.add_parser('add',
-        description='moves an untracked file into your %(prog)s dotfile directory',
-        help='start tracking a new file')
-add_parser.add_argument('-f', '--file', nargs='+', required=True, help='files to start managing')
-add_parser.add_argument('-p', '--package', required=True, help='package to add to')
-add_parser.set_defaults(func=actions.add)
+    # Create subparsers
+    subparsers = parser.add_subparsers(help='action to take', dest='action')
 
-# Parser for update option
-update_parser = subparsers.add_parser('update',
-        description='runs filters and updates a given package',
-        help='filter and update a package')
-update_parser.add_argument('-p', '--package', help='package to filter and update')
-update_parser.set_defaults(func=actions.update)
+    # Parser for add action
+    add_parser = subparsers.add_parser('add',
+            description='moves an untracked file into your %(prog)s dotfile directory',
+            help='start tracking a new file')
+    add_parser.add_argument('-f', '--file', nargs='+', required=True, help='files to start managing')
+    add_parser.add_argument('-p', '--package', required=True, help='package to add to')
+    add_parser.set_defaults(func=actions.add)
 
-# Parse arguments
-args = parser.parse_args()
-if not args.action:
-    parser.error(message='Missing action')
-args.func(args)
+    # Parser for update option
+    update_parser = subparsers.add_parser('update',
+            description='runs filters and updates a given package',
+            help='filter and update a package')
+    update_parser.add_argument('-p', '--package', help='package to filter and update')
+    update_parser.set_defaults(func=actions.update)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Fail if missing an action
+    if not args.action:
+        parser.error(message='Missing action')
+
+    # Initialize config
+    config.initialize(args.config)
+
+    # Call action
+    return args.func(args)
+
+if __name__ == '__main__':
+    sys.exit(main())
